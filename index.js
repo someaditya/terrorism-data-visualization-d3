092893dc4365e0b133e7588662e380e4e95647c9
Some Aditya Mandal <somaditya432@gmail.com>
2018-09-22 18:28:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,41 @@ linechart()
 chorddiagram()
 sankeydiagram()
 
+var randomColor = (function(){
+  var golden_ratio_conjugate = 0.618033988749895;
+  var h = Math.random();
+
+  var hslToRgb = function (h, s, l){
+      var r, g, b;
+
+      if(s == 0){
+          r = g = b = l; // achromatic
+      }else{
+          function hue2rgb(p, q, t){
+              if(t < 0) t += 1;
+              if(t > 1) t -= 1;
+              if(t < 1/6) return p + (q - p) * 6 * t;
+              if(t < 1/2) return q;
+              if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+              return p;
+          }
+
+          var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+          var p = 2 * l - q;
+          r = hue2rgb(p, q, h + 1/3);
+          g = hue2rgb(p, q, h);
+          b = hue2rgb(p, q, h - 1/3);
+      }
+
+      return '#'+Math.round(r * 255).toString(16)+Math.round(g * 255).toString(16)+Math.round(b * 255).toString(16);
+  };
+  
+  return function(){
+    h += golden_ratio_conjugate;
+    h %= 1;
+    return hslToRgb(h, 0.5, 0.60);
+  };
+})();
 
 
 function showHeatmap(low,high)
@@ -115,10 +150,7 @@ var g = svg1.append("g")
 
 d3.csv("data/datafinal.csv", function(error, csv_data) { 
     var data = d3.nest()
-    .key(function(d) { 
-	    if (data.iyear>=low && data.iyear<=high) 
-  	    {
-	    return d.iyear;}})
+    .key(function(d) { return d.iyear;})
     .rollup(function(d) { 
      return {
       kill: d3.sum(d, function(g) {return parseInt(g.nkill); }),
@@ -161,7 +193,7 @@ d3.csv("data/datafinal.csv", function(error, csv_data) {
       .attr("y", function(d) { return y(d.kills); })
       .attr("width", x.bandwidth())
       .attr("height", function(d) { return height - y(d.kills); });
-
+      .style({fill: randomColor});
 });
 }
 
