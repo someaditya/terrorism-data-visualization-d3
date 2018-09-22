88334176f96d9015d0c1430c899e81fea3798c61
Some Aditya Mandal <somaditya432@gmail.com>
2018-09-22 18:03:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -405,17 +405,17 @@ d3.csv("data/datafinal.csv", function(error, csv_data) {
       .attr("d", valueline2);
 
  	svg2.append("text")
-		.attr("transform", "translate(" + (width+3) + "," + y(data[data.length-1].nkill) + ")")
-		.attr("dy", ".35em")
-		.attr("text-anchor", "start")
-		.style("fill", "red")
-		.text("Killed");
-
-	svg2.append("text")
-		.attr("transform", "translate(" + (width+3) + "," + y(data[data.length-1].nwound) + ")")
+		.attr("transform", "translate(" + (width) + "," + y(data[data.length-1].nkill) + ")")
 		.attr("dy", ".35em")
 		.attr("text-anchor", "start")
 		.style("fill", "steelblue")
+		.text("Killed");
+
+	svg2.append("text")
+		.attr("transform", "translate(" + (width) + "," + y(data[data.length-1].nwound) + ")")
+		.attr("dy", ".35em")
+		.attr("text-anchor", "start")
+		.style("fill", "red")
 		.text("Wounded");
 
 
